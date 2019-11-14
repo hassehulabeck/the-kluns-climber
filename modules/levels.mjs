e97ec259b990e755261e6078e5be42505efaae5a
hassehulabeck <hansbudandersson@gmail.com>
2019-11-14 22:00:28 +0100

    Added levels module

---
+++ modules/levels.mjs
@@ -0,0 +1,13 @@
+export default levels = [{
+        backgroundColor: "rgba(200, 100, 30, .2)",
+        foe: 1
+    },
+    {
+        backgroundColor: "rgba(200, 100, 30, .4)",
+        foe: 0
+
+    }, {
+        backgroundColor: "rgba(200, 100, 30, .6)",
+        foe: 2
+    }
+];
