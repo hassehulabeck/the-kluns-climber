fc91d3d28d641c81d7fb20a209f336c5abc92b10
hassehulabeck <hansbudandersson@gmail.com>
2019-11-15 14:58:49 +0100

    Got move animation to work nice

--- modules/hero.mjs
+++ modules/hero.mjs
@@ -13,13 +13,39 @@ function moveHero(direction) {
     for (let i = 0; i <= 100; i += 5) {
         setTimeout(() => {
             heroImg.style.opacity = 1 - (i / 100);
-
+            if (i > 95)
+                renderHero();
         }, 10 * i)
     }
 }
 
 
+function renderHero() {
+    // First, remove old hero.
+    let oldHero = document.getElementById("hero");
+    oldHero.parentNode.removeChild(oldHero);
+
+    let sections = document.getElementsByTagName("section");
+
+    let img = document.createElement("img");
+    img.setAttribute("src", Hero.hero.avatar);
+    img.setAttribute("alt", Hero.hero.name);
+    img.setAttribute("title", Hero.hero.name);
+    img.id = "hero";
+    img.style.opacity = 0;
+
+    sections[hero.level].appendChild(img);
+
+    // Fade in
+    for (let i = 0; i <= 100; i += 1) {
+        setTimeout(() => {
+            img.style.opacity = 0 + (i / 100);
+        }, 11 * i)
+    }
+
+}
 export {
     hero,
-    moveHero
+    moveHero,
+    renderHero
 };
